58dc10b90209cc8547f6426e3296e7dc9b6eaa4b
nestorbecerra11 <142368098+nestorbecerra11@users.noreply.github.com>
2024-01-23 16:47:32 -0500

    Update giphy.service.ts

--- app/giphy.service.ts
+++ app/giphy.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiphyService {
+  private apiKey = '29cjPh4a6sX1AYFu2NZwdoLnn5k51Ipl'; // Reemplaza con tu API key
+  private apiUrl = 'https://api.giphy.com/v1/gifs/search';
 
-  apiKey = '29cjPh4a6sX1AYFu2NZwdoLnn5k51Ipl';
-  apiUrl = 'https://api.giphy.com/v1/randomid';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-    getGifs(searchTerm: string) {  
+  getGifs(searchTerm: string): Observable<any[]> {
     const url = `${this.apiUrl}?api_key=${this.apiKey}&q=${searchTerm}`;
-    return this.http.get(url);
+    return this.http.get<any[]>(url);
   }
-
 }
-
 
